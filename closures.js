e1a0fdb47ea84e9184aceeabcd36408330941c4f
raiguard <caleb.heuer@gmail.com>
2020-06-16 15:45:46 -0600

    Add closures answers

--- closures.js
+++ closures.js
@@ -1,4 +1,4 @@
-/* 
+/*
   Once you complete a problem, refresh ./closures.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
@@ -10,34 +10,30 @@
 
 // Do not edit the code below.
 function outer() {
-  var name = 'Tyler';
-  return function() {
-    return 'The original name was ' + name;
+  var name = "Tyler";
+  return function () {
+    return "The original name was " + name;
   };
 }
 // Do not edit the code above.
-  
-/* 
+
+/*
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
-
+let inner = outer();
 
 //Once you do that, invoke inner.
 
-//Code Here
-
-
+inner();
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
 function callFriend(name) {
   function dial(number) {
-    return 'Calling ' + name + ' at ' + number
+    return "Calling " + name + " at " + number;
   }
   return dial;
 }
@@ -46,14 +42,13 @@ function callFriend(name) {
 /*
   Above you're given a callFriend function that returns the dial function.
   Store the result of invoking callFriend in a variable named callJake.
-  
-  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
+
+  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248'
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
-
-
+const callJake = callFriend("Jake");
+callJake("435-555-9248");
 
 ////////// PROBLEM 3 //////////
 
@@ -61,18 +56,17 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
-
-
+function makeCounter() {
+  let sum = 0;
+  return () => ++sum;
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 ////////// PROBLEM 4 //////////
 
@@ -81,46 +75,41 @@ function callFriend(name) {
   The first function is called inc, this function is responsible for incrementing the value once and returning the updated value.
   The second function is called dec, this function is responsible for decrementing the value by one and returning the updated value.
   You will need to use the module pattern to achieve this.
-  Information on the module pattern available here: 
+  Information on the module pattern available here:
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
 function counterFactory(value) {
-  // Code here.
-
   return {
-
+    inc: () => ++value,
+    dec: () => --value
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
-
-
+counter.inc(); // 11
+counter.inc(); // 12
+counter.inc(); // 13
+counter.dec(); // 12
 
 ////////// PROBLEM 5 //////////
 
 /*
   Inside the motivation function create another function called message that will return the welcome text with the firstname and lastname.
-  The final message should say "You're doing awesome, keep it up firstname lastname." 
+  The final message should say "You're doing awesome, keep it up firstname lastname."
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
+function motivation(firstname, lastname) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  const message = () => `${welcomeText} ${firstname} ${lastname}.`;
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
-var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-
-
+var greeting = motivation("Billy", "Bob"); // 'You're doing awesome keep it up Billy Bob.
 
 ////////// PROBLEM 6 //////////
 
@@ -129,25 +118,23 @@ var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up B
   Invoke this by calling module.publicMethod(); outside the module scope
 */
 
-var module = (function() {
+var module = (function () {
   var person = {
     name: "phillip",
     age: 29,
     location: "Utah"
   };
 
-  function privateMethod(){
+  function privateMethod() {
     return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
   }
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: () => privateMethod()
   };
 })();
-
-
 
 ////////// PROBLEM 7 //////////
 
@@ -162,14 +149,13 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: (num) => (secret += num),
+    takeAwayFromSecret: (num) => (secret -= num)
   };
 }
 
-
-
 ////////// PROBLEM 8 //////////
-  
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -182,14 +168,15 @@ function secretNumber() {
 
   However, because each call to console.log occurs after the loop has finished, the value of i has changed before the console.log executes.
   We'll need to use a closure to preserve a reference to i at the time of execution.
-  
+
   Fix the code below to log the desired output.
 */
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
+    const currentI = i;
+    setTimeout(function () {
+      return console.log(currentI);
     }, i * 1000);
   }
 }
